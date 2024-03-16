5b9aaa22fcdcec1ab9c49733ce86371e7f12828d
raphaa94 <163655419+raphaa94@users.noreply.github.com>
2024-03-16 16:42:47 -0300

    Add files via upload

---
+++ meuambiente2.py
@@ -0,0 +1,31 @@
+print('Hello, django girls')
+name = "Fernanda"
+lottery = [2, 4, 6, 8, 10]
+lottery
+lottery.sort()
+print(lottery[0])
+lottery.pop(0)
+print(lottery)
+participant = {'name': 'hello', 'jujuba' : 'django', 'numeros_favoritos' : [1, 2, 3]}
+print(participant['name'])
+len(participant)
+3>2
+
+def soma(list):
+    list[0] + list[1]
+    print(list[0] + list[1])
+soma(lottery)
+x=10
+y=10
+if(x > y):
+    print("Maria")
+elif(x==y):
+    print("João")
+else:
+    print("Bruxa")
+
+girls = ['Mariana', 'Fernanda', 'Raphaella', 'Ola', 'Voce']
+
+i=0
+for i in girls:
+    print(i)
